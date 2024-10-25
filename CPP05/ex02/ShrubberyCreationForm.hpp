6223a727ac16b4db64fb72f3a9968a540f3ca2f4
Vicente Darras <vdarras@shi-r9-p1.s19.be>
2024-10-25 15:03:29 +0200

    f

---
+++ CPP05/ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,6 @@
+#ifndef __SHRUBBERY__
+# define __SHRUBBERY__
+
+# include "AForm.hpp"
+
+#endif
--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -5,32 +5,6 @@
 
 int main()
 {
-    Bureaucrat bu(80, "Bu");
-    Form form("Form", 100, 100);
-    Form form0("Form0", 70, 70);
-    try {
-        Form form1("Form1", 20, 20);
-        Form form2("Form2", 156, 140); // EXCEPTION
-    }
-    catch(std::exception &e) {
-        std::cout << "Error: " << e.what() << std::endl;
-    }
-    std::cout << "--------------------------------------------------" << std::endl;
-    try {
-        form.beSigned(bu);
-        form0.beSigned(bu); // Exception
-    }
-    catch(std::exception &e) {
-        std::cout << "Error: " << e.what() << std::endl;
-    }
-    std::cout << "--------------------------------------------------" << std::endl;
-
-    bu.signForm(form);
-    bu.signForm(form0);
-    
-    std::cout << "--------------------------------------------------" << std::endl;
-    std::cout << "Overload = operator of Bureaucrat: \n" << bu << "\n" <<  std::endl;
-    std::cout << "Overload = operator of Form: \n" << form << std::endl;
 
     return 0;
 }
